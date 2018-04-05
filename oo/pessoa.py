c9a7c6acd353ef8098af1ee46b0a86a6c1360b0e
genascimento <geni1004>
2018-04-05 20:16:20 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome =None, idade=42):
+    def __init__(self, *filhos, nome=None, idade=42):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Reijane')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Genilton'
-    print(p.nome)
-    print(p.idade)
+    joao = Pessoa(nome='Joao')
+    genilton  = Pessoa(joao, nome='Genilton')
+    print(Pessoa.cumprimentar(genilton))
+    print(id(genilton))
+    print(genilton.cumprimentar())
+    print(genilton.nome)
+    print(genilton.idade)
+    for filho in joao.filhos:
+        print(filho.nome)
